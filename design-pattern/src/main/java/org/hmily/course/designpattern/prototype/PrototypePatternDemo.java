cd2b603e1076044740161b3c45ac0ab9ac40c982
You-Hmily <706097141@qq.com>
2019-11-14 19:17:18 +0800

    添加原型模式 demo

---
+++ design-pattern/src/main/java/org/hmily/course/designpattern/prototype/PrototypePatternDemo.java
@@ -0,0 +1,19 @@
+package org.hmily.course.designpattern.prototype;
+
+public class PrototypePatternDemo {
+
+
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape cloneShape1 = ShapeCache.getShape("1");
+        System.out.println("Shape : "+cloneShape1.getType());
+
+        Shape cloneShape2 = ShapeCache.getShape("2");
+        System.out.println("Shape : "+cloneShape2.getType());
+
+        Shape cloneShape3 = ShapeCache.getShape("3");
+        System.out.println("Shape : "+cloneShape3.getType());
+    }
+
+}
---
+++ design-pattern/src/main/java/org/hmily/course/designpattern/prototype/Rectangle.java
@@ -0,0 +1,15 @@
+package org.hmily.course.designpattern.prototype;
+
+public class Rectangle extends Shape{
+
+    public Rectangle(){
+        type = "Rectangle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle:draw() method.");
+    }
+
+
+}
